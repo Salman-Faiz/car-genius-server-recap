77e52c842acaedf95e9d6ec0b4217baa0396ec96
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-09-04 12:44:14 +0600

    find specific data from using get operation

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.listen(port, () => {
 // **********
 // ***********
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 // import from dotenv npm site
 require("dotenv").config();
 console.log(process.env.CAR_PASS);
@@ -45,8 +46,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // create a collection to find data
+    // created a collection to find data
     const serviceCollection = client.db("carGenius").collection("Services");
+
     // find data UNDER an api
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -54,6 +56,24 @@ async function run() {
       res.send(result);
     });
 
+    // Find a specific data
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      //
+      const options = {
+        // Include only the `title` and `services` fields in the returned document
+        projection: { title: 1, service_id: 1, price: 1, img: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+    // *****************************************
+    // *****************************************
+    // *********************************************
+    // **************************************
+    //
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
